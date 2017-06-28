e39558c82b3e7e1e5ff293c1921516bb63c85de5
Stephen Oney <swloney@gmail.com>
2017-06-28 02:33:30 -0400

    adding autoload

--- auth.php
+++ auth.php
@@ -1,8 +1,10 @@
 <?php
-$app_id = "359042"
-$app_key = "44f522b8df5a8a3ccf74"
-$app_secret = "d8d561e9aafb45f2ff14"
-$app_cluster = "us2"
+require('vendor/autoload.php');
+
+$app_id = "359042";
+$app_key = "44f522b8df5a8a3ccf74";
+$app_secret = "d8d561e9aafb45f2ff14";
+$app_cluster = "us2";
 
 global $user;
 if ($user->uid) {
